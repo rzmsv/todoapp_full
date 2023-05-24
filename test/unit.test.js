adf69446ab4c268ade671bdfdace562bc2127a36
reza.mousavi <reza.mousavi@partsoftware.com>
2023-05-24 04:57:26 -0400

    task unit test done

--- test/unit.test.js
+++ test/unit.test.js
@@ -6,12 +6,17 @@ const {Todo} = require("../src/models")
 const DUMMY_DATA = {
     task: "new task"
 }
+const UPDATE_DUMMY_DATA = {
+    task: "new update task"
+}
 const WRONG_DUMMY_DATA = {
     test: "new task"
 }
 
-beforAll(async()=>{
-
+afterAll(async()=>{
+    await Todo.destroy({
+        where: {}
+      })
 })
 
 describe("App unit test",()=>{
@@ -46,12 +51,44 @@ describe("App unit test",()=>{
     })
     describe("Patch method",()=>{
         it("Should update task.",async()=>{
+            const getTask = await Todo.findAll()
+            const {id} = getTask[0].dataValues
             await request(app)
-            .patch('/api/v1/todo/update/1')
+            .patch(`/api/v1/todo/update/${id}`)
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .send(DUMMY_DATA)
+            .send(UPDATE_DUMMY_DATA)
             .expect(200)
+        })
+    })
+    describe("Patch method",()=>{
+        it("Should not update task.( not found! )",async()=>{
+            const id = 1
+            await request(app)
+            .patch(`/api/v1/todo/update/${id}`)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .send(UPDATE_DUMMY_DATA)
+            .expect(404)
+        })
+    })
+    describe("Delete method",()=>{
+        it("Should delete task.",async()=>{
+            const getTask = await Todo.findAll()
+            const {id} = getTask[0].dataValues
+            await request(app)
+            .delete(`/api/v1/todo/delete/${id}`)
+            .send(UPDATE_DUMMY_DATA)
+            .expect(200)
+        })
+    })
+    describe("Delete method",()=>{
+        it("Should not delete task.( not found! )",async()=>{
+            const id = 1
+            await request(app)
+            .delete(`/api/v1/todo/delete/${id}`)
+            .send(UPDATE_DUMMY_DATA)
+            .expect(404)
         })
     })
 
