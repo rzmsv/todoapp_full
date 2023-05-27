e754aeec30fa6b1fbe98963f33edaea5c07c87d1
reza.mousavi <reza.mousavi@partsoftware.com>
2023-05-27 08:12:05 -0400

    write unit test for auth api

--- test/unit/auth/auth.test.js
+++ test/unit/auth/auth.test.js
@@ -1,15 +1,11 @@
-const { JsonWebTokenError } = require("jsonwebtoken")
-const {AuthController} = require("../../../src/controllers/auth.controller")
+const app = require("../../../src")
+const request = require("supertest")(app)
 const {User} = require("../../../src/models")
 const { hashPassword } = require("../../../src/utils/bcrypt.utils")
-const {asyncHandler} = require("../../../src/helper/asyncHandler.helper")
 
 /* ----------------------------------- --- ---------------------------------- */
 
-const response = {
-    status: jest.fn(x => x),
-    json: jest.fn(x => x)
-}
+
 
 beforeAll(async()=>{
     const hash = await hashPassword("password")
@@ -28,32 +24,83 @@ afterAll(async()=>{
 
 describe("Auth test .", () => {
     it("should login",async () => {
-        const request = {
-            body:{
-                email: "reza@gmail.com",
-                password: "password"
-            }
+        const data = {
+            email: "reza@gmail.com",
+            password: "password"
         }
-        await AuthController.login_controller(request,response)
-        expect(response.status).toHaveBeenCalledWith(200)
-        expect(response.status).toHaveBeenCalledTimes(1)
+
+        const response = await request
+        .post('/api/v1/auth/login')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .send(data)
+       
+        expect(response.status).toBe(200)
     })
     
     it("User not found for login",async () => {
-        const request = {
-            body:{
-                email: "rezaa@gmail.com", // Email wrong .
-                password: "password"
-            }
+        const data = {
+            email: "rez@gmail.com", // Email wrong .
+            password: "password"
         }
-        const res =AuthController.login_controller(request,response)
+
+        const response = await request
+        .post('/api/v1/auth/login')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .send(data)
+       
+        expect(response.body.message).toBe("User not found!")
+        expect(response.status).toBe(404)
+    })
+
+    it("User password wrong",async () => {
+        const data = {
+            email: "reza@gmail.com", 
+            password: "passwordd" // Password wrong .
+        }
+
+        const response = await request
+        .post('/api/v1/auth/login')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .send(data)
+       
         
-        console.log(res)
-        // console.log("----------")
-        // console.log(res)
-        // console.log("-----------")
-        // expect(response.status).toHaveBeenCalledWith(200)
-        // expect(response.status).toHaveBeenCalledTimes(1)
-    
+        expect(response.body.message).toBe("Password wrong!")
+        expect(response.status).toBe(400)
+    })
+
+    it("Email was empty!",async () => {
+        const data = {
+            email:'' , 
+            password: 'password'
+        }
+
+        const response = await request
+        .post('/api/v1/auth/login')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .send(data)
+       
+        
+        expect(response.body.message).toEqual(["Insert email!"])
+        expect(response.status).toBe(400)
+    })
+    it("password was empty",async () => {
+        const data = {
+            email:'reza@gmail.com' , 
+            password: ''
+        }
+
+        const response = await request
+        .post('/api/v1/auth/login')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .send(data)
+       
+        
+        expect(response.body.message).toEqual(["Insert password!"])
+        expect(response.status).toBe(400)
     })
 })
