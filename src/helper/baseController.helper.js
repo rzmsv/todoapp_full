475426d35937efa170dd5d07353088f52fa0bf30
reza.mousavi <reza.mousavi@partsoftware.com>
2023-05-27 06:18:26 -0400

    create auth and login api write unit test for login write e2e test

--- src/helper/baseController.helper.js
+++ src/helper/baseController.helper.js
@@ -1,13 +1,18 @@
 exports.BaseController = class {
 
     static ok(res,result={},status=200){
-        return res.status(status).json({
+        const statusCode = res.status(status)
+        const response = res.json({
             statusCode:status,
             code:"OK",
             message:result
         })
+        return statusCode,response
+        
     }
     static fail(res,err){
-        return res.status(err.statusCode).json({ statusCode: err.statusCode, code: err.code, message: err.message })
+        const statusCode = res.status(err.statusCode)
+        const response = res.json({ statusCode: err.statusCode, code: err.code, message: err.message })
+        return statusCode,response
     }
 }
--- test/unit/auth/auth.test.js
+++ test/unit/auth/auth.test.js
@@ -1,6 +1,59 @@
+const { JsonWebTokenError } = require("jsonwebtoken")
+const {AuthController} = require("../../../src/controllers/auth.controller")
+const {User} = require("../../../src/models")
+const { hashPassword } = require("../../../src/utils/bcrypt.utils")
+const {asyncHandler} = require("../../../src/helper/asyncHandler.helper")
+
+/* ----------------------------------- --- ---------------------------------- */
+
+const response = {
+    status: jest.fn(x => x),
+    json: jest.fn(x => x)
+}
+
+beforeAll(async()=>{
+    const hash = await hashPassword("password")
+    await User.create({
+        email: "reza@gmail.com",
+        password: hash
+    })
+    
+})
+afterAll(async()=>{
+    await User.destroy({
+        where: {}
+    })
+})
+
 
 describe("Auth test .", () => {
-    it("should login", () => {
-
+    it("should login",async () => {
+        const request = {
+            body:{
+                email: "reza@gmail.com",
+                password: "password"
+            }
+        }
+        await AuthController.login_controller(request,response)
+        expect(response.status).toHaveBeenCalledWith(200)
+        expect(response.status).toHaveBeenCalledTimes(1)
+    })
+    
+    it("User not found for login",async () => {
+        const request = {
+            body:{
+                email: "rezaa@gmail.com", // Email wrong .
+                password: "password"
+            }
+        }
+        const res =AuthController.login_controller(request,response)
+        
+        console.log(res)
+        // console.log("----------")
+        // console.log(res)
+        // console.log("-----------")
+        // expect(response.status).toHaveBeenCalledWith(200)
+        // expect(response.status).toHaveBeenCalledTimes(1)
+    
     })
 })
